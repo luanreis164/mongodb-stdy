b894b68b3ced7b36eb9877c3a66b799a6be22de4
Luan Reis <luanreis2202@gmail.com>
2022-04-11 23:10:12 -0300

    Criando o projeto e criando Entidade User e UserResource

---
+++ src/main/java/com/lr/mongodbstd/MongodbstdApplication.java
@@ -0,0 +1,13 @@
+package com.lr.mongodbstd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongodbstdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongodbstdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lr/mongodbstd/MongodbstdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lr.mongodbstd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongodbstdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
