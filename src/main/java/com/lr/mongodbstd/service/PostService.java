3bfa3e8aa3ab1cab8dfcffef3eca836f8ba0b241
Luan Reis <luanreis2202@gmail.com>
2022-04-13 22:03:49 -0300

    Consulta com vários critérios

--- src/main/java/com/lr/mongodbstd/service/PostService.java
+++ src/main/java/com/lr/mongodbstd/service/PostService.java
@@ -8,6 +8,7 @@ import com.lr.mongodbstd.service.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -16,13 +17,22 @@ public class PostService {
 
     @Autowired
     private PostRepository repo;
+
     //Metodo de busca por email
     public Post findById(String id){
         Optional<Post> post = repo.findById(id);
         return post.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
+
     //Metodo de busca por texto
     public List<Post> findByTitle(String text){
         return repo.findByTitleContainingIgnoreCase(text);
     }
+
+    //Metodo de busca completa por texto e datas de postagem
+    public List<Post> fullSearch(String text, Date minDate, Date maxDate){
+        maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+        return repo.fullSearch(text, minDate, maxDate);
+    }
+
 }
